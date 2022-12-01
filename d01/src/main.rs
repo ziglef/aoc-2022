d1869fc3f9304e87b184168009221eaea2529901
Grandao <ruigrandaorocha@gmail.com>
2022-12-01 11:31:04 +0000

    post solution cleanup

--- d01/src/main.rs
+++ d01/src/main.rs
@@ -1,40 +1,24 @@
 fn main() {
-    let mut maxs: [i32; 3] = [0,0,0];
-    let mut elf_sum: i32 = 0;
+    let mut elfs: Vec<u32> = Vec::new();
+    let mut elf_sum: u32 = 0;
 
     include_str!("../input.txt")
         .lines()
         .for_each(|i| {
-            let amt: i32 = i.parse::<i32>().unwrap_or(0);
+            let amt: u32 = i.parse::<u32>().unwrap_or(0);
 
             if amt != 0 {
                 elf_sum += amt;
             } else {
-                println!("{}", elf_sum);
-                for (index, value) in maxs.clone().iter().enumerate() {
-                    if elf_sum > *value {
-                        if index < maxs.len() - 1 {
-                            if *value != 0 {
-                                for (index, value) in maxs.clone().iter().enumerate().rev() {
-                                    if index != maxs.len() - 1 {
-                                        maxs[index+1] = *value;
-                                    }
-                                }
-                            }
-                        }
-                        maxs[index] = elf_sum;
-                        break;
-                    }
-                }
-                println!("{:?}", maxs);
+                elfs.push(elf_sum);
                 elf_sum = 0;
-            }
-
-            
+            } 
         });
 
+    elfs.sort();
     println!(
-        "{:?}\nsum: {}",
-        maxs, maxs.iter().sum::<i32>()
+        "top elf: {}\nsum: {}",
+        elfs.last().unwrap(),
+        elfs.iter().rev().take(3).sum::<u32>()
     );
 }
