f3e14112848a8decbc1112016d992e974993f542
Grandao <ruigrandaorocha@gmail.com>
2022-12-03 09:52:29 +0000

    More cleanup, we now just read each line into a vec and then do the split only for part1, this reduces work for part2

--- d03/src/main.rs
+++ d03/src/main.rs
@@ -1,34 +1,30 @@
 use std::collections::HashSet;
 
 fn main() {
-    let mut rucksacks: Vec<(&str, &str)> = Vec::new();
+    let rucksacks: Vec<&str> = include_str!("../input.txt").lines().collect();
     let mut priorities: Vec<char> = Vec::new();
     let mut badges: Vec<char> = Vec::new();
-
-    include_str!("../input.txt")
-        .lines()
-        .for_each(|i| {
-            rucksacks.push(i.split_at(i.len()/2));
-
-            let items: HashSet<char> = rucksacks.last().unwrap().0.chars().collect();
-            for c in rucksacks.last().unwrap().1.chars() {
-                if items.contains(&c) {
-                    priorities.push(c);
-                    break;
-                }
-            }
-        });
-
+    
     // First problem
+    for rucksack in &rucksacks {
+        let compartments = rucksack.split_at(rucksack.len()/2);
+        let items: HashSet<char> = compartments.0.chars().collect();
+        for c in compartments.1.chars() {
+            if items.contains(&c) {
+                priorities.push(c);
+                break;
+            }
+        }
+    }
     println!(
         "{}",
         priorities.iter().map(|&p| if p.is_uppercase() { p as u32 - 'A' as u32 + 27 } else { p as u32 - 'a' as u32 + 1 }).sum::<u32>(),
     );
 
     for group_rucksacks in rucksacks.chunks(3) {
-        let mut items: HashSet<char> = format!("{}{}", group_rucksacks[0].0, group_rucksacks[0].1).chars().collect();
-        let items2: HashSet<char> = format!("{}{}", group_rucksacks[1].0, group_rucksacks[1].1).chars().collect();
-        let items3: HashSet<char> = format!("{}{}", group_rucksacks[2].0, group_rucksacks[2].1).chars().collect();
+        let mut items: HashSet<char> = group_rucksacks[0].chars().collect();
+        let items2: HashSet<char> = group_rucksacks[1].chars().collect();
+        let items3: HashSet<char> = group_rucksacks[2].chars().collect();
         
         items.retain(|item| items2.contains(item) && items3.contains(item));
         badges.push(*items.iter().last().unwrap());
